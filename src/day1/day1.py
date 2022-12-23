1b4221c4480a561250f7b5efcac8265f0232408b
Luca Giudici <luca.giudici@intre.it>
2022-12-23 12:22:34 +0100

    ✨ Day 1

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -2,15 +2,41 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Elf:
+    def __init__(self, calories: List[str]):
+        self.carriedCalories = calories.copy()
+
+    def totalCalories(self):
+        return sum([int(cal) for cal in self.carriedCalories])
 
 
-# Press the green button in the gutter to run the script.
+def getLinesOfFile(filename: str):
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
+    return lines
+
+
+def getElvesFromFile():
+    lines = getLinesOfFile('src/day1/input.txt')
+    tmp = []
+    elves = []
+    for line in lines:
+        if line == "":
+            elves.append(Elf(tmp))
+            tmp.clear()
+        else:
+            tmp.append(line)
+    return elves
+
+
+def getMaxCalories(elves: List[Elf], n=1):
+    return sorted([elf.totalCalories() for elf in elves], reverse=True)[0:n]
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    elves = getElvesFromFile()
+    print(f"elf carrying max calories is carrying {getMaxCalories(elves)}")
+    print(f"top 3 carrying calories are carrying {getMaxCalories(elves,3)} sum is {sum(getMaxCalories(elves,3))}")
